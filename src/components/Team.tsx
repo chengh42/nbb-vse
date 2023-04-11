4c9bc58d6bb26962d762f9b6aba0cc0b54fe244a
chengh42 <chenghsin42@gmail.com>
2023-04-11 21:54:24 +0200

    Fix placeholder

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -2,19 +2,22 @@ import {Team} from "@/types/Team";
 import Image from "next/image";
 
 export default function Team({afko, logo, positie}: Team) {
+  const placeholder = <p className="relative block w-16 h-16">{afko}</p>;
   return (
     <div className="flex flex-row justify-between items-center w-32 p-2 m-4">
       <p className="text-lg">{positie}</p>
-      <div className="relative block w-16 h-16">
-        {logo === "" ? null : (
+      {logo.length === 0 ? (
+        placeholder
+      ) : (
+        <div className="relative block w-16 h-16 hover:w-20 hover:h-20 hover:transition-all">
           <Image
             src={logo}
             alt={afko}
             fill
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
           />
-        )}
-      </div>
+        </div>
+      )}
     </div>
   );
 }
