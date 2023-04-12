cfe2e32801382bf6937f7193c8ecd83bb3fbca0e
Hsin <chenghsin42@gmail.com>
2023-04-12 09:25:01 +0200

    Refactor standing page, add placeholder teams page (#3)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,108 +1,36 @@
-import {useEffect, useState} from "react";
-import Image from "next/image";
 import {Inter} from "next/font/google";
 
-import {League} from "@/types/League";
-import {Standing} from "@/types/Standing";
-import {Team as TeamProps} from "@/types/Team";
-
-import _LEAGUES from "../data/LEAGUES.json";
-import Team from "@/components/Team";
 import Loader from "@/components/Loader";
 import Navbar from "@/components/Navbar";
+import Link from "next/link";
 
 const inter = Inter({subsets: ["latin"]});
 
-type LeagueGroups = {[leagueGroup: string]: League[]};
-const LEAGUES = _LEAGUES as LeagueGroups;
-type LeagueGroupName = keyof LeagueGroups;
-
-const LeagueBoard = ({
-  isLoading,
-  teams,
-}: {
-  isLoading: boolean;
-  teams: TeamProps[] | undefined;
-}): JSX.Element => {
-  if (isLoading) {
-    return <Loader />;
-  }
-  if (!teams) {
-    return <p>Cannot load standing data for this league.</p>;
-  }
-  return (
-    <div>
-      {teams.map((team) => (
-        <Team key={team.ID} {...team} />
-      ))}
-    </div>
-  );
-};
-
 export default function Home() {
-  const leagueGroupNames = Object.keys(LEAGUES) as LeagueGroupName[];
-  const [leagueGroupName, setLeagueGroupName] = useState(leagueGroupNames[0]);
-  const leagues: Array<League> = LEAGUES[leagueGroupName];
-  const [league, setLeague] = useState<League>(leagues[0]);
-  const [standing, setStanding] = useState<Standing | null>(null);
-  const [isLoading, setIsLoading] = useState(false);
-
-  const LeagueGroupMenu = () => (
-    <select
-      className="text-lg rounded-lg p-4 m-2"
-      value={leagueGroupName}
-      onChange={(ev) => {
-        setLeagueGroupName(ev.target.value);
-        setLeague(LEAGUES[leagueGroupName][0]);
-      }}
-    >
-      {leagueGroupNames.map((leagueGroupName) => (
-        <option key={leagueGroupName} value={leagueGroupName}>
-          {leagueGroupName}
-        </option>
-      ))}
-    </select>
-  );
-
-  const LeagueMenu = () => (
-    <select
-      className="text-lg rounded-lg p-4 m-2"
-      value={league.id}
-      onChange={(ev) => {
-        const seletedLeagueId = Number(ev.target.value);
-        const seletedLeague = leagues.find(({id}) => seletedLeagueId === id);
-        if (seletedLeague) {
-          setLeague(seletedLeague);
-        } else {
-          alert(`Cannot load league for unknown id ${seletedLeagueId}`);
-        }
-      }}
-    >
-      {leagues.map(({id, name}) => (
-        <option key={id} value={id}>
-          {name}
-        </option>
-      ))}
-    </select>
-  );
-
-  useEffect(() => {
-    setIsLoading(true);
-    fetch(`/api/standing/${league.id}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setStanding(data);
-        setIsLoading(false);
-      });
-  }, [league]);
-
+  const buttonClassName =
+    "relative inline-flex items-center w-full px-4 py-2 font-medium hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white";
   return (
     <>
       <Navbar />
       <main className="flex min-h-screen flex-col items-center justify-start p-24">
-        <LeagueGroupMenu />
-        <LeagueMenu />
-        <LeagueBoard isLoading={isLoading} teams={standing?.stand} />
+        <ul>
+          <li>
+            <Link href="/standing" className={buttonClassName}>
+              <span className="material-symbols-outlined mr-1">
+                leaderboard
+              </span>
+              Standing
+            </Link>
+          </li>
+          <li>
+            <Link href="/team/123" className={buttonClassName}>
+              <span className="material-symbols-outlined mr-1">
+                sports_basketball
+              </span>
+              Teams
+            </Link>
+          </li>
+        </ul>
       </main>
     </>
   );
